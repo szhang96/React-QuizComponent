28ad207df6b358be51247322270562d41d9315d1
Shirley Zhang <shirleyzhang2078@gmail.com>
2018-07-10 13:39:26 -0400

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <li>{this.props.quiz_questions.answer_options[0]} </li>{" "}
+            <li>{this.props.quiz_question.answer_options[0]} </li>{" "}
           </ul>
         </section>
       </main>
