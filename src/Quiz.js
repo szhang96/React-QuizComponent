ade02974b0fec05d4c3b43f9dff362b6d1c74421
Shirley Zhang <shirleyzhang2078@gmail.com>
2018-07-10 15:49:17 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,13 +17,16 @@ class Quiz extends React.Component {
       return { quiz_position: state.quiz_position + 1 };
     });
   }
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
