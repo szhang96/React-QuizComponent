3067c3b7ef5693df6238aa983dc6932854a5076b
Shirley Zhang <shirleyzhang2078@gmail.com>
2018-07-10 12:19:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
-
+import Quiz from './components/Quiz'
 class App extends Component {
   render() {
     return (
-      <div />
-    )
+      <div>
+      <Quiz/>
+      </div>
+    );
   }
 }
 
